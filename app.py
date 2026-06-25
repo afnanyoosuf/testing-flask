c7168d6a51afd55dfdf45886533a764ce22add11
Afnan Yoosuf <afnanyoosuf3@gmail.com>
2026-06-25 09:45:36 +0530

    ready to ci

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hi, Flask"
+
+@app.route("/about")
+def about():
+    return "About Page"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({
+            "error": "No JSON data provided"
+        }), 400
+
+    if "age" not in data:
+        return jsonify({
+            "error": "Age is required"
+        }), 400
+
+    if not isinstance(data["age"], int):
+        return jsonify({
+            "error": "Age must be a number"
+        }), 400
+
+    age = data["age"]
+
+    if age < 0 or age > 100:
+        return jsonify({
+            "error": "Age must be between 0 and 100"
+        }), 400
+
+    if age < 18:
+        result = "child"
+    else:
+        result = "adult"
+
+    return jsonify({
+        "prediction": result
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
---
+++ test_app.py
@@ -0,0 +1,58 @@
+from app import app
+import pytest
+
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_valid_adult(client):
+    response = client.post(
+        '/predict',
+        json={"age": 22}
+    )
+
+    assert response.status_code == 200
+    assert response.json["prediction"] == 'adult'
+
+
+def test_valid_child(client):
+    response = client.post(
+        '/predict',
+        json={"age": 5}
+    )
+
+    assert response.status_code == 200
+    assert response.json["prediction"] == 'child'
+
+
+def test_missing_age(client):
+    response = client.post(
+        '/predict',
+        json={}
+    )
+
+    assert response.status_code == 400
+    assert response.json["error"] == "Age is required"
+
+
+def test_invalied_type(client):
+    response = client.post(
+        '/predict',
+        json={"age": "aabc"}
+    )
+
+    assert response.status_code == 400
+    assert response.json["error"] == "Age must be a number"
+
+
+def test_invalied_range(client):
+    response = client.post(
+        '/predict',
+        json={"age": -5}
+    )
+
+    assert response.status_code == 400
+    assert response.json["error"] == "Age must be between 0 and 100"
